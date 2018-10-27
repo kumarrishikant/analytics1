891e68d72a410ab2ddd1a833cb7ab05582fd12ed
kumarrishikant <rishi290890@gmail.com>
2018-10-27 18:11:45 +0530

    27/10/2018

--- cluster_of_marks1_marks2_plot.R
+++ cluster_of_marks1_marks2_plot.R
@@ -2,8 +2,17 @@ marks1 = ceiling(rnorm(10, 6,2))
 marks2 = ceiling(rnorm(10, 5,2))
 df = data.frame(marks1, marks2)
 head(df)
-km = kmeans(df, centers=2)
+km = kmeans(df, centers = 2)
 km$cluster
-df2=cbind(df, cluster=km$cluster)
+df2=cbind(df, cluster = km$cluster)
 df2[order(df2$cluster),]
-plot(df$marks1, df$marks2, col=km$cluster)
+plot(df$marks1, df$marks2, col=2:3, pch=c(16,17))
+head(data)
+summary(data)
+str(data)
+#verify the data
+colnames(data)
+sapply(data, class)
+class(data$age)
+
+
