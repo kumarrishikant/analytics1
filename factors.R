b0ba8f14d0b099faeca90f83a756c7c3c087296c
kumarrishikant <rishi290890@gmail.com>
2018-10-27 12:46:49 +0530

    27/10/2018

--- factors.R
+++ factors.R
@@ -6,12 +6,13 @@ genderF = factor(gender)
 summary(genderF)
 genderF
 
-(likescale = sample(c('Ex','Good','Sat','Poor'),size=20,replace=T))
-summary(likescaleF)
-class(likescaleF)
-likescaleOF = factor(likescale,ordered=T,levels=c('poor','sat','good','Ex'))
-summary(likescaleOF)
-likescaleOF
-barplot(table(likescaleF)), col=1:4
-barplot(table(likescaleOF)), col=1:4
+(likscale = sample(c('Ex','Good','Sat','Poor'), size=20,replace=T))
+summary(likscaleF)
+class(likscaleF)
+likscaleOF = factor(likscale, ordered=T, levels=c('poor', 'sat','good' ,'Ex'))
+summary(likscaleOF)
+likscaleOF
+barplot(table(likscaleF), col=1:4)
+barplot(table(likscaleOF), col=1:4)
+
 
